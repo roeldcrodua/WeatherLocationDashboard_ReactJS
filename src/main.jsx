0a84e3e524047898331219154fff0092bbd6550c
Roel Crodua <roelcrodua@yahoo.com>
2025-11-02 13:41:05 -0500

    Added route page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import Layout from './routes/Layout'
+import Dashboard from './routes/Dashboard'
+import ChartsView from './routes/ChartsView'
+import DetailView from './routes/DetailView'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="/charts/:location" element={<ChartsView />} />
+          <Route path="/details/:name/:lat/:lon" element={<DetailView />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
